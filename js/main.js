b333255ee6b54c5617e3420ad8635fb92f10d0ce
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-06-01 15:29:10 -0300

    header pronto

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// //função que carrega o preloader
+// $(window).load(function () {
+//   $('#preloader .inner').fadeOut();
+//   $('#preloader').delay(350).fadeOut('slow'); 
+//   $('body').delay(350).css({'overflow': 'visible'});
+// })
