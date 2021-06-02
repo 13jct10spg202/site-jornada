f0b88237a750bd4b59bdd11ec3377989e2f6f329
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-06-02 20:04:15 -0300

    scroll top funcionando

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
-//função que carrega o preloader
-// $(window).load(function () {
-//   alert("aaaaaaa")
-//   // $('#preloader .inner').fadeOut();
-//   // $('#preloader').delay(350).fadeOut('slow'); 
-//   // $('body').delay(350).css({'overflow': 'visible'});
-// })
-
 $(document).ready(function () {
-  // cronometro
-var DataLimite = new Date('Jul 6, 2021 00:00:00').getTime();
 
-function CalculaTempo(){
-  var agora = new Date().getTime(); // pegando o tempo de agora
+
+  // cronometro
+  var DataLimite = new Date("Jul 6, 2021 00:00:00").getTime();
+
+  function CalculaTempo() {
+    var agora = new Date().getTime(); // pegando o tempo de agora
     intervalo = DataLimite - agora;
 
     var segundos = 1000;
@@ -19,39 +13,42 @@ function CalculaTempo(){
     var horas = minutos * 60;
     var dias = horas * 24;
 
-    var d = Math.floor(intervalo/(dias));
-    var h = Math.floor((intervalo % (dias))/(horas));
-    var m = Math.floor((intervalo % (horas))/(minutos));
-    var s = Math.floor((intervalo %(minutos))/segundos);
+    var d = Math.floor(intervalo / dias);
+    var h = Math.floor((intervalo % dias) / horas);
+    var m = Math.floor((intervalo % horas) / minutos);
+    var s = Math.floor((intervalo % minutos) / segundos);
 
-    document.getElementById('dia').innerHTML = d;
-    document.getElementById('horas').innerHTML = h;
-    document.getElementById('minutos').innerHTML = m;
-    document.getElementById('segundos').innerHTML = s;  
-}
-setInterval(function(){
-  CalculaTempo();
-},1000)
-
-
-
-//botão de scroll para o topo
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    
-    $('#scrollBtn').addClass('show');
-  } else {
-    $('#scrollBtn').removeClass('show');
+    document.getElementById("dia").innerHTML = d;
+    document.getElementById("horas").innerHTML = h;
+    document.getElementById("minutos").innerHTML = m;
+    document.getElementById("segundos").innerHTML = s;
   }
+  setInterval(function () {
+    CalculaTempo();
+  }, 1000);
+
+  //botão de scroll para o topo
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      $("#scrollBtn").addClass("show");
+    } else {
+      $("#scrollBtn").removeClass("show");
+    }
+  });
+
+  $("#scrollBtn").click(function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    return false;
+  });
+
+  //Animação de preloader no site
+  //<![CDATA[
+  $(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow'); 
+    $('body').delay(350).css({'overflow': 'visible'});
+  })
+  //]]>
+
+
 });
-
-$("#scrollBtn").click(function() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-  return false;
-});
-
-});
-
-
-
-
